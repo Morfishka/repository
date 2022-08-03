599213eef420a27513c38216b0af8b0c3fdaae28
Morfishka <salimyanova.tany@mail.ru>
2022-08-03 14:29:34 +0500

    project05_особое приветствие

---
+++ project05/Program.cs
@@ -0,0 +1,13 @@
+﻿// Особое приветствие
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower == "Татьяна");
+{
+    Console.WriteLine("Ух ты! Та самая Татьяна!");
+}   
+else
+{    
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
